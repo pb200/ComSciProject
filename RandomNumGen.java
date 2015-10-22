452b8caba33d0ff0ca4baf27586a775d230b4b8a
pb200 <blakeyphillip@gmail.com>
2015-10-21 21:21:45 -0400

    Update RandomNumGen.java

--- RandomNumGen.java
+++ RandomNumGen.java
@@ -3,9 +3,27 @@ import java.util.Random;
 
 
 public class RandomNumGen {
+	//private long seed;
+	private Random rand1;
+	public RandomNumGen(){
+		//this.seed = Seed;
+		//System.out.println("Making random with seed " + seed);
+		this.rand1 = new Random();
+		
+	}
+	
+	
 	public double getRandomNum(){
-		Random rand = new Random();
-		double randomNum = rand.nextDouble();
+		double randomNum = rand1.nextDouble();
+		//System.out.println(randomNum);
 		return randomNum;
 	}
+	//public static void main(String [] args){
+		//RandomNumGen one = new RandomNumGen((long)Math.pow(2, 30));
+		//RandomNumGen two = new RandomNumGen(0L);
+		//double t11=one.getRandomNum();
+		//double t12 = one.getRandomNum();
+		//double t13 = two.getRandomNum();
+		//System.out.println(t11 + " , " + t12 + " , " + t13);
+	//}
 }
