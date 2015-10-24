06c95ef6a0fddec0c1f2f281bae457b0b8e14c08
pb200 <blakeyphillip@gmail.com>
2015-10-24 13:12:18 -0400

    Update MonteCarloSim.java

--- MonteCarloSim.java
+++ MonteCarloSim.java
@@ -1,16 +1,38 @@
 //Copyright (c) <2015> <Phillip Blakey>
 import java.util.Timer;
 
-public class MonteCarloSim {
+public class MonteCarloSim implements Runnable{
 	static Timer timer = new Timer();
 	static int seconds = 0;
-	public static void main(String [] args){
-		Statistics stats= new Statistics();
-		TrialRunner instance = new TrialRunner(10000000,2);
-		NVarFunction nVarF = new NVarFunction();
-		final long startTime = System.currentTimeMillis();
-		instance.runNVar(nVarF);
-		final long endTime = System.currentTimeMillis();
-		System.out.println("Total execution time: " + (endTime - startTime) + " milliseconds");
+	private long seed = 0;
+	private int trials = 0;
+	private int numVars = 0;
+	public MonteCarloSim(long seedIn, int trialsIn, int numVarsIn){
+		this.seed = seedIn;
+		this.trials = trialsIn;
+		this.numVars = numVarsIn;
 	}
+	 public void run()
+	    {
+	      {
+	        try
+	        {
+	          Thread.sleep(1000);
+	        }
+	        catch (InterruptedException ie)
+	        {
+	          System.err.println("Interrupted");
+	          return;
+	        }
+	        System.out.println("Thread ID "+Thread.currentThread().getId()+" running");
+	        //Statistics stats= new Statistics();
+			TrialRunner instance = new TrialRunner(this.trials,this.numVars,this.seed);
+			NVarFunction nVarF = new NVarFunction();
+			final long startTime = System.currentTimeMillis();
+			instance.runNVar(nVarF);
+			final long endTime = System.currentTimeMillis();
+			System.out.println("Total execution time: " + (endTime - startTime) + " milliseconds");
+
+	      }
+	    }
 }
