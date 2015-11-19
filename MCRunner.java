f0da651487a9a473abdd8632dbd6ad4c40b4dea1
pb200 <blakeyphillip@gmail.com>
2015-11-19 17:21:47 -0500

    Update MCRunner.java

--- MCRunner.java
+++ MCRunner.java
@@ -1,74 +1,78 @@
 package com.amazonaws.codesamples.gsg;
+
 //Copyright (c) <2015> <Phillip Blakey>
 import java.security.NoSuchAlgorithmException;
-
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.Table;
-import com.amazonaws.services.dynamodbv2.document.spec.UpdateItemSpec;
-import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
 
 public class MCRunner {
 	private int numThreads = 0;
-	private double pi;
 	private int numVars = 0;
+	private int numTrials = 0;
 	GenerateUUID seedArray;
-	public MCRunner(int numThreadsIn, int numVarsIn) throws NoSuchAlgorithmException{
+
+	public MCRunner(int numThreadsIn, int numTrialsIn, int numVarsIn) throws NoSuchAlgorithmException {
 		this.numThreads = numThreadsIn;
 		this.numVars = numVarsIn;
-	seedArray = new GenerateUUID(numThreadsIn);
+		this.numTrials = numTrialsIn;
+		seedArray = new GenerateUUID(numThreadsIn);
 	}
-	public int getNumThreads(){
+
+	public int getNumThreads() {
 		return this.numThreads;
 	}
-	public int getNumVars(){
+
+	public int getNumVars() {
 		return this.numVars;
 	}
-	public GenerateUUID getSeedArray(){
+
+	public GenerateUUID getSeedArray() {
 		return this.seedArray;
 	}
-	public static void main(String [] args) throws NoSuchAlgorithmException, InterruptedException{
-		
-		MCRunner mCRunner = new MCRunner(4,2);
-		int numThreads = mCRunner.getNumThreads();
 
-		Thread [] threads = new Thread[numThreads];
+	public void runMC() throws NoSuchAlgorithmException, InterruptedException {
+		Thread[] threads = new Thread[numThreads];
 		MonteCarloSim[] mcs = new MonteCarloSim[numThreads];
-		
+
 		AmazonDynamoDBClient client = new AmazonDynamoDBClient();
 		client.setEndpoint("http://localhost:8000");
 		DynamoDB dynamoDB = new DynamoDB(client);
-		
 		Table table = dynamoDB.getTable("Pi_Statistics");
-		
-		
-		for (int i = 0; i < numThreads; i++){
-			mcs[i] = new MonteCarloSim(mCRunner.getSeedArray().getSeed(i),1000000,2);
+
+		for (int i = 0; i < numThreads; i++) {
+			mcs[i] = new MonteCarloSim(seedArray.getSeed(i), numTrials, 2);
 			threads[i] = new Thread(mcs[i]);
 			threads[i].start();
 		}
-		
-		for(int i = 0; i < numThreads; i++) {
-			table.putItem(new Item()
-			.withPrimaryKey("Seed", mCRunner.getSeedArray().getSeed(i))
-			.withDouble("Total_excecution_timePB", mcs[i].getExcecutionTime())
-			.withDouble("TimePB", mcs[i].getEndTime()));
-			for(int j = 0; j < mCRunner.getNumVars(); j++){
-				 UpdateItemSpec updateItemSpec = new UpdateItemSpec()
-		            .withPrimaryKey("Seed", mCRunner.getSeedArray().getSeed(i))
-		            .withUpdateExpression("set info.StandardDeviation"+j+
-		            		" = :s, info.Variance"+j+"=:V, info.Mean"+j+"=:m")
-		            .withValueMap(( new ValueMap())
-		                .withNumber(":s", mcs[i].getStandardDeviation(j))
-		                .withNumber(":V", mcs[i].getVarience(j))
-		                .withNumber(":m", mcs[i].getMean(j)));
-
-			}
+		for (int i = 0; i < numThreads; i++) {
 			threads[i].join();
 		}
-		
-		double pi = mcs[numThreads-1].getSumPi()/numThreads;
-		System.out.println("Pi = " + pi);
+		for (int i = 0; i < numThreads; i++) { // TODO (phil) Check thread lifetimes
+			String std = "";
+			String mean = "";
+			String varience = "";
+			for (int j = 0; j < numVars; j++) {
+				std = std + ", " + mcs[i].getStandardDeviation(j);													// num vars
+				mean = mean + ", " + mcs[i].getMean(j);
+				varience = varience + ", " + mcs[i].getVarience(j);
+			}
+			
+// TODO (phil) Do a unit test to see the difference between toString and what I do in terms of length
+			System.out.println("MCRunner Seed0!: " + seedArray.getSeed(0)); 
+			System.out.println("MCRunner Seed: " + seedArray.getSeed(i));
+			System.out.println("Excecution time: " + mcs[i].getExcecutionTime());
+			System.out.println("End time: " + mcs[i].getEndTime());
+			table.putItem(new Item().withPrimaryKey("Seed", seedArray.getSeed(i))
+					.withDouble("Total_excecution_timePB", mcs[i].getExcecutionTime())
+					.withDouble("TimePB", mcs[i].getEndTime())
+					.withString("Std", std)
+					.withString("Mean", mean)
+					.withString("Varience", varience)
+					.withDouble("Successes", mcs[i].getSuccesses())
+					.withNumber("Trials", mcs[i].getNumTrials()));
+
+		}
 	}
 }
